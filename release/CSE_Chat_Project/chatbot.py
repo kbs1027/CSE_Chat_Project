d85cc24cb242ae1f744225762937ad0824b16a7c
kbs1027 <rlaqjatn1027@gmail.com>
2023-05-06 18:40:00 +0900

    update(23.05.06)

--- release/CSE_Chat_Project/chatbot.py
+++ release/CSE_Chat_Project/chatbot.py
@@ -12,6 +12,9 @@ import win32clipboard
 from PIL import Image
 import logging
 import logging.handlers
+import os
+import chromedriver_autoinstaller as AutoChrome
+import shutil
 
 LOG_MAX_SIZE = 1024 *1024 * 10
 
@@ -29,9 +32,54 @@ file_handler = logging.handlers.RotatingFileHandler('log/CSE_Chatbot.log',maxByt
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-chatnames = ['C:/Users/nuc/project/CSE_Chat_Project/src/PNU_ChatBot.PNG']
-#chatnames = ['C:/Users/nuc/project/CSE_Chat_Project/src/test.PNG']
+#chatnames = ['src/PNU_ChatBot.PNG']
+chatnames = ['src/test.PNG']
 
+
+def driverUpdate():
+    try:
+        chrome_list = []
+        chrome_ver = AutoChrome.get_chrome_version().split('.')[0]
+        current_list = os.listdir(os.getcwd())
+        for i in current_list:
+            path = os.path.join(os.getcwd(), i)
+            if os.path.isdir(path):
+                if 'chromedriver.exe' in os.listdir(path):
+                    chrome_list.append(i)
+        old_version = list(set(chrome_list)-set([chrome_ver]))
+        for i in old_version:
+            path = os.path.join(os.getcwd(),i)
+            shutil.rmtree(path)
+        if not chrome_ver in current_list:
+            logger.info(f'DriverUpdate-Before{old_version} => Latest[{chrome_ver}]')
+            AutoChrome.install(True)
+        else :
+            logger.info(f'DriverUpdate-Release[{chrome_ver}]')
+            pass
+    except Exception as e:
+        logger.error("UpdateError")
+        logger.error(e)
+        pass
+
+def loadDriver():
+    global driver
+    chrome_ver = AutoChrome.get_chrome_version().split('.')[0]
+    path = os.path.join(os.getcwd(),chrome_ver)
+    path = os.path.join(path,'chromedriver.exe')
+
+    options = Options()
+    options.add_argument("lang=ko_KR")
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--window-size=1280,720')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36')
+    driver = webdriver.Chrome(str(path), options=options)
+    driver.set_page_load_timeout(15) # timeout 10 sec
+    logger.info(f'LoadDriver-Release[{chrome_ver}]')
+    return driver
+    
 def sendContent(path):
     image = Image.open(path)
 
@@ -41,7 +89,7 @@ def sendContent(path):
     output.close()
 
     send_to_clipboard(win32clipboard.CF_DIB, data)
-    chat_btn = pyautogui.locateCenterOnScreen('C:/Users/nuc/project/CSE_Chat_Project/src/chatblock.PNG', confidence=0.8)
+    chat_btn = pyautogui.locateCenterOnScreen('src/chatblock.PNG', confidence=0.8)
     pyautogui.moveTo(chat_btn.x, chat_btn.y)
     pyautogui.doubleClick()
     pyautogui.hotkey("ctrl","v")
@@ -57,18 +105,6 @@ def send_to_clipboard(clip_type, data):
     win32clipboard.CloseClipboard()
 
 def macroSend(chatname_path, contents):
-    options = Options()
-    options.add_argument("lang=ko_KR")
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument('--window-size=1280,720')
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36')
-    
-    driver = webdriver.Chrome(options=options)
-
-    driver.set_page_load_timeout(10)
     SendCheck = False
     for i in range(3):
         logger.info(f'==try count:[{i+1}]')
@@ -77,17 +113,17 @@ def macroSend(chatname_path, contents):
             pyautogui.moveTo(room_btn.x, room_btn.y)
             pyautogui.rightClick()
             time.sleep(1)
-            roomopen_btn = pyautogui.locateCenterOnScreen('C:/Users/nuc/project/CSE_Chat_Project/src/chatopen.PNG', confidence=0.9)
+            roomopen_btn = pyautogui.locateCenterOnScreen('src/chatopen.PNG', confidence=0.9)
             pyautogui.moveTo(roomopen_btn.x, roomopen_btn.y)
             pyautogui.doubleClick()
             time.sleep(1)
-            chat_btn = pyautogui.locateCenterOnScreen('C:/Users/nuc/project/CSE_Chat_Project/src/chatblock.PNG', confidence=0.9)
+            chat_btn = pyautogui.locateCenterOnScreen('src/chatblock.PNG', confidence=0.9)
             pyautogui.moveTo(chat_btn.x, chat_btn.y)
             pyautogui.doubleClick()
             pyperclip.copy(contents)
             pyautogui.hotkey("ctrl","v")
             time.sleep(1)
-            send_btn = pyautogui.locateCenterOnScreen('C:/Users/nuc/project/CSE_Chat_Project/src/send.PNG', confidence=0.9)
+            send_btn = pyautogui.locateCenterOnScreen('src/send.PNG', confidence=0.9)
             pyautogui.moveTo(send_btn.x, send_btn.y)
             pyautogui.doubleClick()
             logger.info("macroSendMessage")
@@ -103,25 +139,14 @@ def macroSend(chatname_path, contents):
         raise AirflowException('retry gogo!')
 
 def closeChat():
-    close_btn = pyautogui.locateCenterOnScreen('C:/Users/nuc/project/CSE_Chat_Project/src/close.PNG')
+    close_btn = pyautogui.locateCenterOnScreen('src/close.PNG')
     pyautogui.moveTo(close_btn.x, close_btn.y)
     pyautogui.doubleClick()
     logger.info("closeChat")
     
 def screenshot(URL,Type):
-    options = Options()
-    options.add_argument("lang=ko_KR")
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument('--window-size=1280,720')
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36')
-    
-    driver = webdriver.Chrome(options=options)
-
-    driver.set_page_load_timeout(10) # timeout 10 sec
-
+    global driver
+    dirver = loadDriver()
     crawling_success = False
     for i in range(5):
         logger.info(f'ScreenShot==try count:[{i+1}]')
@@ -136,7 +161,8 @@ def screenshot(URL,Type):
     if crawling_success == False:
         logger.error("failSendScreenshot")
         raise AirflowException('retry gogo!') # Airflow Exception
-    screenshot = driver.save_screenshot(f'{Type}.png')
+    driver.save_screenshot(f'{Type}.png')
+    del driver
     
 def Load(Case):
     if Case == "Notice" :
@@ -267,7 +293,7 @@ def Notice():
                             pass
                         try:
                             screenshot(Notice_new_state["link"],'Notice')
-                            filepath = 'C:/Users/nuc/project/CSE_Chat_Project/Notice.png'
+                            filepath = 'Notice.png'
                             sendContent(filepath)
                         except Exception as e:
                             logger.error("ScreenshotError")
@@ -317,7 +343,7 @@ def Free():
                             pass
                         try:
                             screenshot(Free_new_state["link"],'Free')
-                            filepath = 'C:/Users/nuc/project/CSE_Chat_Project/Free.png'
+                            filepath = 'Free.png'
                             sendContent(filepath)
                         except Exception as e:
                             logger.error("ScreenshotError")
@@ -366,7 +392,7 @@ def Employoment():
                             pass
                         try:
                             screenshot(Employment_new_state["link"],'Employment')
-                            filepath = 'C:/Users/nuc/project/CSE_Chat_Project/Employment.png'
+                            filepath = 'Employment.png'
                             sendContent(filepath)
                         except Exception as e:
                             logger.error("ScreenshotError")
@@ -416,7 +442,7 @@ def Contest():
                             pass
                         try:
                             screenshot(Contest_new_state["link"],'Contest')
-                            filepath = 'C:/Users/nuc/project/CSE_Chat_Project/Contest.png'
+                            filepath = 'Contest.png'
                             sendContent(filepath)
                         except Exception as e:
                             logger.error("ScreenshotError")
@@ -466,7 +492,7 @@ def Others():
                             pass
                         try:
                             screenshot(Others_new_state["link"],'Others')
-                            filepath = 'C:/Users/nuc/project/CSE_Chat_Project/Others.png'
+                            filepath = 'Others.png'
                             sendContent(filepath)
                         except Exception as e:
                             logger.error("ScreenshotError")
@@ -484,6 +510,9 @@ def Others():
         logger.error(e)
         pass
         
+        
+        
+schedule.every().day.at("00:00").do(driverUpdate)
 schedule.every(300).seconds.do(Contest)
 schedule.every(300).seconds.do(Employoment)
 schedule.every(300).seconds.do(Free)
